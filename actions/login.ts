c177aa42a9942f1e8662c26e1ced2432da0e51ed
summer030 <125527872+summer030@users.noreply.github.com>
2024-03-27 18:48:06 +0800

    feat: middleware & login

--- actions/login.ts
+++ actions/login.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import * as z from "zod";
+import { AuthError } from "next-auth";
 
+import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
+import { DEFAULT_LOGGIN_REDIRECT } from "@/routes";
 
 export const login = async (
   values: z.infer<typeof LoginSchema>
@@ -13,5 +16,22 @@ export const login = async (
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password } = validatedFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+    throw error;
+  }
 };
