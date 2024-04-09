ce6ae08fd2a6b7cef8f8d124c1f88f90d0b3656e
summer030 <125527872+summer030@users.noreply.github.com>
2024-04-09 18:18:16 +0800

    feat: OAuth(Google & Github)

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useTransition } from "react";
 import * as z from "zod";
+import { useSearchParams } from "next/navigation";
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -24,6 +25,11 @@ import { Button } from "@/components/ui/button";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 
 const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get("error") === "OAuthAccountNotLinked"
+      ? "Email already in use with different provider!"
+      : "";
   const [isPending, startTransition] = useTransition();
   const [error, setError] = useState<string | undefined>(
     ""
@@ -47,8 +53,9 @@ const LoginForm = () => {
       setError("");
       setSuccess("");
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        // TODO: Add when we add 2FA
+        // setSuccess(data.success);
       });
     });
   };
@@ -102,7 +109,7 @@ const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
           <Button
             type="submit"
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,18 +1,25 @@
 "use client";
 
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGGIN_REDIRECT,
+    });
+  };
   return (
     <div className="w-full flex items-center gap-x-2">
       <Button
         variant="outline"
         size="lg"
         className="w-full"
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className="h-5 w-5" />
       </Button>
@@ -20,7 +27,7 @@ export const Social = () => {
         variant="outline"
         size="lg"
         className="w-full"
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <FaGithub className="h-5 w-5" />
       </Button>
