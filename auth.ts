18b43c019117ea7bbc9b765b4e3a61f61132844b
summer030 <125527872+summer030@users.noreply.github.com>
2024-05-05 23:18:18 +0800

    feat: server & client example

--- auth.ts
+++ auth.ts
@@ -63,6 +63,10 @@ export const {
       if (token.role && session.user) {
         session.user.role = token.role as UserRole;
       }
+      if (session.user) {
+        session.user.isTwoFactorEnabled =
+          token.isTwoFactorEnabled as boolean;
+      }
 
       return session;
     },
@@ -74,6 +78,8 @@ export const {
       if (!existingUser) return token;
 
       token.role = existingUser.role;
+      token.isTwoFactorEnabled =
+        existingUser.isTwoFactorEnabled;
 
       return token;
     },
